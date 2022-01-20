2cbbf7694429e311237dbf24a3c2bb31758bf971
nekozye <nekozeye@gmail.com>
2022-01-19 23:51:05 -0500

    added a button listener

--- app/src/main/java/com/neko/todolistapplication/MainActivity.kt
+++ app/src/main/java/com/neko/todolistapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.neko.todolistapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // add button listener check
+        findViewById<Button>(R.id.buttonAddTask).setOnClickListener {
+
+            //executed when button is pressed
+            Log.i("todolist_button","User Clicked on button")
+        }
     }
 }
