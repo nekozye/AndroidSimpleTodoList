634daf6dfe045923aebec6b601902a3b5eaa5c16
nekozye <nekozeye@gmail.com>
2022-01-21 18:49:56 -0500

    implemented a recyclerview. Todo:need to actually make the add button update the sequence

--- app/src/main/java/com/neko/todolistapplication/MainActivity.java
+++ app/src/main/java/com/neko/todolistapplication/MainActivity.java
@@ -1,21 +1,28 @@
 package com.neko.todolistapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> listOfTasks;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
 
+        listOfTasks = new ArrayList<String>();
 
+
+        //button stuff
         View buttonView = findViewById(R.id.buttonAddTask);
         View.OnClickListener ocl = new View.OnClickListener() {
             @Override
@@ -25,6 +32,20 @@ public class MainActivity extends AppCompatActivity {
         };
 
         buttonView.setOnClickListener(ocl);
+
+
+        listOfTasks.add("Test Value 1");
+        listOfTasks.add("Test Value 2");
+
+
+        //recylcler stuff
+
+        RecyclerView rcyView = findViewById(R.id.recyclerfield);
+        TaskItemAdapter taskAdapter = new TaskItemAdapter(listOfTasks);
+
+
+        rcyView.setAdapter(taskAdapter);
+        rcyView.setLayoutManager(new LinearLayoutManager(this));
 
     }
 }
