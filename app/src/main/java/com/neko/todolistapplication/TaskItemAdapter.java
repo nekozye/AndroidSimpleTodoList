30989a6ae277cad3e119d7e29ea61b8a8414a33a
nekozye <nekozeye@gmail.com>
2022-01-21 18:25:58 -0500

    TaskItemAdapter class all implemented

--- app/src/main/java/com/neko/todolistapplication/TaskItemAdapter.java
+++ app/src/main/java/com/neko/todolistapplication/TaskItemAdapter.java
@@ -1,4 +1,65 @@
 package com.neko.todolistapplication;
 
-public class TaskItemAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class TaskItemAdapter extends RecyclerView.Adapter<TaskItemAdapter.ViewHolder>{
+
+    private List<String> taskitems;
+
+    public TaskItemAdapter(List<String> tasks){
+        this.taskitems = tasks;
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textview;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            this.textview = itemView.findViewById(android.R.id.text1);
+        }
+    }
+
+
+    @Override
+
+    //need to inflate a layout from XML and returning the holde.
+    public TaskItemAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        //custom or premade layout, inflate.
+        View listView = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+
+        return new ViewHolder(listView);
+    }
+
+    @Override
+    public void onBindViewHolder( TaskItemAdapter.ViewHolder holder, int position) {
+        //Get the data model based on position
+
+        String currentItemContents = taskitems.get(position);
+
+        //Set item views based on the views and data model
+
+        holder.textview.setText(currentItemContents);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return taskitems.size();
+    }
+
+
 }
